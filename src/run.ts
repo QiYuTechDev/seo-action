09b1d06ca880a7af9b4e00c88b4be1361e6b7e4d
dev <dev@qiyutech.tech>
2021-09-24 21:26:37 +0800

    macOS & windows mongodb

--- src/run.ts
+++ src/run.ts
@@ -1,17 +1,28 @@
 import os from "os"
+import * as core from "@actions/core"
 import {cliRun} from "./cli";
 
 function runLinux() {
+    core.info("install mongodb")
     cliRun("xvfb-run", ["/usr/lib/seo/seo", "--no-sandbox", "--help"])
 }
 
 function runMacOS() {
+    core.info("install mongodb")
+    cliRun("brew", ["tap", "mongodb/brew"])
+    cliRun("brew", ["install", "mongodb-community@5.0"])
+    core.info("start mongodb")
+    cliRun("brew", ["services", "start", "mongodb-community"])
+    core.info("start seo")
     cliRun("./seo.app/Contents/MacOS/seo", ["--help"])
 }
 
 function runWin32() {
+    core.info("install mongodb")
+    cliRun("choco", ["install", "mongodb"])
     cliRun("whoami")
     cliRun("ls", ["c:\\\\users\\\\runneradmin\\\\AppData\\\\Local\\\\seo"])
+    core.info("start seo")
     cliRun("c:\\users\\runneradmin\\AppData\\Local\\seo\\seo.exe", ["--help"])
 }
 
