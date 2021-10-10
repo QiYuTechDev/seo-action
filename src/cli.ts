d41bf24cfc1c3b39c1dd06840f539a2c8695e208
dev <dev@qiyutech.tech>
2021-10-10 21:58:22 +0800

    add debug

--- src/cli.ts
+++ src/cli.ts
@@ -7,18 +7,24 @@ import * as child_process from "child_process";
  * @param args
  */
 export function cliRun(cli: string, args: string[] | null = null) {
+    const debug = core.getBooleanInput("debug")
+
     core.info(`${cli} ${(args || []).join(" ")}`)
     let ret = child_process.spawnSync(cli, args || [])
     if (ret.status !== 0) {
-        core.warning(`stdout: ${ret.stdout}`)
-        core.error(`stderr: ${ret.stderr}`)
+        if (debug) {
+            core.warning(`stdout: ${ret.stdout}`)
+            core.error(`stderr: ${ret.stderr}`)
+        }
         if (cli === "xpra") {
             core.warning("xpra exec failed")
         } else {
             core.setFailed(`exec ${cli} failed`)
         }
     } else {
-        core.info(ret.stdout.toString())
-        core.warning(ret.stderr.toString())
+        if (debug) {
+            core.info(ret.stdout.toString())
+            core.warning(ret.stderr.toString())
+        }
     }
 }
