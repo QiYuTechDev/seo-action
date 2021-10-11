76925551e77c180d3a75fa07caeb5e09345033eb
dev <dev@qiyutech.tech>
2021-10-11 11:05:04 +0800

    fix mongodb for linux

--- src/run.ts
+++ src/run.ts
@@ -4,9 +4,9 @@ import {cliRun} from "./cli";
 
 function runLinux() {
     core.info("install mongodb")
-    cliRun("sudo", ["apt", "install", "-y", "mongodb"], true, true)
-    cliRun("sudo", ["apt", "remove", "-y", "mongodb"], true, true)
-    cliRun("sudo", ["apt", "install", "-y", "mongodb"])
+    cliRun("wget", ["-qq", "https://repo.mongodb.org/apt/ubuntu/dists/focal/mongodb-org/5.0/multiverse/binary-amd64/mongodb-org-server_5.0.3_amd64.deb"])
+    cliRun("sudo", ["dpkg", "-i", "mongodb-org-server_5.0.3_amd64.deb"])
+    cliRun("sudo", ["mongod", "--fork", "--port=27019", "-f", "/etc/mongod.conf"])
     cliRun("xpra", ["start", "--start-child=\"seo --mongo-url=mongodb://127.0.0.1:27019 --test-mongo-server\"", "--bind-tcp=127.0.0.1:28182", "--html=off", "--exit-with-children", "--daemon=off"], true, true)
     cliRun("xpra", ["start", "--start-child=\"seo --mongo-url=mongodb://127.0.0.1:27019\"", "--bind-tcp=127.0.0.1:28182", "--html=on", "--exit-with-children", "--daemon=on"])
 }
