6d68de0b69aab8d3c487ee9aaacf2636072fa4e4
dev <dev@qiyutech.tech>
2021-10-12 21:37:32 +0800

    fix for notice

--- dist/index.js
+++ dist/index.js
@@ -380,15 +380,13 @@ function showNotice(name) {
     const run_id = process.env["GITHUB_RUN_ID"] || "";
     const repo = process.env['GITHUB_REPOSITORY'] || "";
     const gh_token = core.getInput("gh_token");
-    if (!gh_token) {
-        core.info('gh token is not valid(you can not direct view the result)');
-        return;
-    }
     const url = new URL("https://ci.2cc.net");
     url.searchParams.set("run_id", run_id);
     url.searchParams.set("repo", repo);
     url.searchParams.set('name', name);
-    url.searchParams.set("gh_token", gh_token);
+    if (gh_token) {
+        url.searchParams.set("gh_token", gh_token);
+    }
     core.notice(`you can view ${name} by\n:${url.toString()}`);
 }
 exports.showNotice = showNotice;
